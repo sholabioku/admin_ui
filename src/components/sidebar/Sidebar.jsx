8efb8783390085d023ecaed134cbe8bcbf2b5bed
sholabioku <bilushonline@gmail.com>
2022-08-11 17:30:23 +0100

    Complete sidebar menu design

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,17 @@
-import { LineStyle, Timeline, TrendingUp } from '@mui/icons-material';
+import {
+  AttachMoney,
+  BarChart,
+  ChatBubbleOutline,
+  DynamicFeed,
+  LineStyle,
+  MailOutline,
+  PermIdentity,
+  Report,
+  Storefront,
+  Timeline,
+  TrendingUp,
+  WorkOutline,
+} from '@mui/icons-material';
 import './sidebar.css';
 
 const Sidebar = () => {
@@ -19,6 +32,61 @@ const Sidebar = () => {
             <li className='sidebarListItem'>
               <TrendingUp className='sidebarIcon' />
               Sales
+            </li>
+          </ul>
+        </div>
+        <div className='sidebarMenu'>
+          <h1 className='sidebarTitle'>Quick Menu</h1>
+          <ul className='sidebarList'>
+            <li className='sidebarListItem'>
+              <PermIdentity className='sidebarIcon' />
+              Users
+            </li>
+            <li className='sidebarListItem'>
+              <Storefront className='sidebarIcon' />
+              Products
+            </li>
+            <li className='sidebarListItem'>
+              <AttachMoney className='sidebarIcon' />
+              Transactions
+            </li>
+            <li className='sidebarListItem'>
+              <BarChart className='sidebarIcon' />
+              Reports
+            </li>
+          </ul>
+        </div>
+        <div className='sidebarMenu'>
+          <h1 className='sidebarTitle'>Notifications</h1>
+          <ul className='sidebarList'>
+            <li className='sidebarListItem active'>
+              <MailOutline className='sidebarIcon' />
+              Mail
+            </li>
+            <li className='sidebarListItem'>
+              <DynamicFeed className='sidebarIcon' />
+              Feedback
+            </li>
+            <li className='sidebarListItem'>
+              <ChatBubbleOutline className='sidebarIcon' />
+              Messages
+            </li>
+          </ul>
+        </div>
+        <div className='sidebarMenu'>
+          <h1 className='sidebarTitle'>Staff</h1>
+          <ul className='sidebarList'>
+            <li className='sidebarListItem active'>
+              <WorkOutline className='sidebarIcon' />
+              Manage
+            </li>
+            <li className='sidebarListItem'>
+              <Timeline className='sidebarIcon' />
+              Analytics
+            </li>
+            <li className='sidebarListItem'>
+              <Report className='sidebarIcon' />
+              Reports
             </li>
           </ul>
         </div>
