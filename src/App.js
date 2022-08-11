64dcf37e79ec4df582be97d71f2a41ba7afa774f
sholabioku <bilushonline@gmail.com>
2022-08-11 18:29:08 +0100

    Add design for featuredInfo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
 
 import './app.css';
+import Home from './pages/home/Home';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Topbar />
       <div className='container'>
         <Sidebar />
-        <div className='others'>Other pages</div>
+        <Home />
       </div>
     </div>
   );
