6b21642b3849245eaf86b80d4fed235ea0d3740c
sholabioku <bilushonline@gmail.com>
2022-08-12 09:59:58 +0100

    Add design for latest transaction widget

--- src/components/widgetLg/WidgetLg.jsx
+++ src/components/widgetLg/WidgetLg.jsx
@@ -1,7 +1,83 @@
 import './widgetLg.css';
 
 const WidgetLg = () => {
-  return <div className='widgetLg'>WidgetLg</div>;
+  const Button = ({ type }) => {
+    return <button className={'widgetLgButton ' + type}>{type}</button>;
+  };
+
+  return (
+    <div className='widgetLg'>
+      <h3 className='widgetLgTitle'>Latest transactions</h3>
+      <table className='widgetLgTable'>
+        <tr className='widgetLgTr'>
+          <th className='widgetLgTh'>Customer</th>
+          <th className='widgetLgTh'>Date</th>
+          <th className='widgetLgTh'>Amount</th>
+          <th className='widgetLgTh'>Status</th>
+        </tr>
+        <tr className='widgetLgTr'>
+          <td className='widgetLgUser'>
+            <img
+              src='https://images.pexels.com/photos/4909457/pexels-photo-4909457.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              alt=''
+              className='widgetLgImg'
+            />
+            <span className='widgetLgName'>Susan Carol</span>
+          </td>
+          <td className='widgetLgDate'>2 Jun 2021</td>
+          <td className='widgetLgAmount'>$122.00</td>
+          <td className='widgetLgStatus'>
+            <Button type='Approved' />
+          </td>
+        </tr>
+        <tr className='widgetLgTr'>
+          <td className='widgetLgUser'>
+            <img
+              src='https://images.pexels.com/photos/4909457/pexels-photo-4909457.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              alt=''
+              className='widgetLgImg'
+            />
+            <span className='widgetLgName'>Susan Carol</span>
+          </td>
+          <td className='widgetLgDate'>2 Jun 2021</td>
+          <td className='widgetLgAmount'>$122.00</td>
+          <td className='widgetLgStatus'>
+            <Button type='Declined' />
+          </td>
+        </tr>
+        <tr className='widgetLgTr'>
+          <td className='widgetLgUser'>
+            <img
+              src='https://images.pexels.com/photos/4909457/pexels-photo-4909457.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              alt=''
+              className='widgetLgImg'
+            />
+            <span className='widgetLgName'>Susan Carol</span>
+          </td>
+          <td className='widgetLgDate'>2 Jun 2021</td>
+          <td className='widgetLgAmount'>$122.00</td>
+          <td className='widgetLgStatus'>
+            <Button type='Pending' />
+          </td>
+        </tr>
+        <tr className='widgetLgTr'>
+          <td className='widgetLgUser'>
+            <img
+              src='https://images.pexels.com/photos/4909457/pexels-photo-4909457.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              alt=''
+              className='widgetLgImg'
+            />
+            <span className='widgetLgName'>Susan Carol</span>
+          </td>
+          <td className='widgetLgDate'>2 Jun 2021</td>
+          <td className='widgetLgAmount'>$122.00</td>
+          <td className='widgetLgStatus'>
+            <Button type='Approved' />
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
 };
 
 export default WidgetLg;
